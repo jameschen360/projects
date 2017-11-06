d6dc2597dadbf996818ff4d039db02941b22ccd9
James Chen <jxchen@live.ca>
2017-11-06 15:32:15 -0700

    db update ***

--- a4hc/inc/db.php
+++ a4hc/inc/db.php
@@ -1,9 +1,9 @@
 <?php
 ob_start();
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'healt495_admin');
-define('DB_PASSWORD', '092104003');
-define('DB_DATABASE', 'healt495_accounting');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '***');
+define('DB_DATABASE', '***');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	mysqli_set_charset($db, "utf8");
     mysqli_query($db, "SET character_set_client=utf8");
--- springbanks/inc/db.php
+++ springbanks/inc/db.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'springbankdelive_james');
-define('DB_PASSWORD', '092104003');
-define('DB_DATABASE', 'springbankdelive_grocery');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '***');
+define('DB_DATABASE', '***');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	mysqli_set_charset($db, "utf8");
     mysqli_query($db, "SET character_set_client=utf8");
