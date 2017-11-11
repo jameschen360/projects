f6e8c2b0d3936fbadcaf253b6208c86e22ce11fa
James Chen <jxchen@live.ca>
2017-11-11 14:59:30 -0700

    processing order cart information properly put

--- spadmin/src/app/home/tables/processing-table/processing-modal/processing-modal.component.ts
+++ spadmin/src/app/home/tables/processing-table/processing-modal/processing-modal.component.ts
@@ -14,12 +14,25 @@ export class ProcessingModalComponent implements OnInit {
   public userDetails: any;
   public token: string;
   processingModalBusy: Promise<any>;
-
   processingModalPostData = {
     'id': '',
     'processing_order_id': '',
     'token': ''
   };
+  // order response parameters
+  userInfo: any;
+  cartInfos: any;
+  productInfos: any;
+  orderInfo: any;
+
+  userFullname: string;
+  userAddress: string;
+  userEmail: string;
+  userPhone: string;
+
+  orderTime: string;
+  deliveryType: string;
+  store: string;
 
   constructor(public dialogRef: MatDialogRef<ProcessingModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any,
     public getData: ProcessingModalService) {
@@ -43,10 +56,21 @@ export class ProcessingModalComponent implements OnInit {
   getProcessingTable() {
     this.processingModalBusy = this.getData.postData(this.processingModalPostData, 'processingModal').then((result) => {
       this.responseData = result;
+      this.userInfo = this.responseData.userInfo;
+      this.cartInfos = this.responseData.cartData;
+      this.productInfos = this.responseData.product;
+      this.orderInfo = this.responseData.orderInfo;
+
+      this.userFullname = this.userInfo.fname + ' ' + this.userInfo.lname;
+      this.userAddress = this.userInfo.address + ' ' + this.userInfo.pcode;
+      this.userEmail = this.userInfo.email;
+      this.userPhone = this.userInfo.phone;
+
+      this.orderTime = this.orderInfo.order_time;
+      this.deliveryType = this.orderInfo.delivery_method;
+      this.store = this.orderInfo.store;
+
       console.log(this.responseData);
-
-
-
 
     }, (err) => {
       // do something if error
